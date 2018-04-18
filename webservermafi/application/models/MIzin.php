a6028e353201bce219685c54a1d891f7cefcfb2e
Kikis <kikismaulana1902@gmail.com>
2018-04-18 09:58:17 +0700

    Nih baru lagi

--- webservermafi/application/models/MIzin.php
+++ webservermafi/application/models/MIzin.php
@@ -17,4 +17,14 @@ class MIzin extends CI_Model {
 		return $this->db->get_where($this->tabel2, 'id_izin != ');
 	}
 
+	public function readdetailizin($id_izin)
+	{
+		return $this->db->get_where($this->tabel2, 'id_izin', $id_izin);
+		// $this->db->select('*');
+		// $this->db->from('v_izin');
+		// $this->db->where('id_izin',$id_izin);
+		// $query = $this->db->order_by('id_izin');
+		// return $query;
+	}
+
 }
---
+++ webservermafi/application/views/admin/pesanbelumdiambil.php
@@ -0,0 +1,42 @@
+
+                    <div class="col-md-9">
+                      <div class="card">
+                        <h5 class="card-header bg-light">Detail</h5>
+                        <div class="card-body">
+                          <p>Belum ada pesan yang di ambil.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="card">
+                        <h5 class="card-header bg-light text-danger">Keterangan : </h5>
+                        <div class="card-body">
+                          <table cellpadding="10px" cellpadding="10px">
+                          <tr>
+                            <td width="50px" height="50px" style="background-color: #FFC107"></td>
+                            <td><h3> : Belum di konfirmasi</h3></td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                            <td width="50px" height="50px" style="background-color: #28A745"></td>
+                            <td><h3> : Sudah di konfirmasi</h3></td>
+                          </tr>
+                        </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /Conten Header-->
